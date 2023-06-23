46bd1718a21c9d2a079b27338cfb3e258c4c1cb6
Afreeda <amahesan@uoguelph.ca>
2023-06-23 09:27:02 -0400

    new URLs form, implement body-parser, randomString

--- express_server.js
+++ express_server.js
@@ -4,14 +4,36 @@ const PORT = 8080; //default port 8080
 
 app.set("view engine", "ejs");
 
+const generateRandomString = function (stringLength) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz';
+
+  let result = '';
+  const charsLength = chars.length;
+  for (let i = 0; i < stringLength; i++) {
+    result += chars.charAt(Math.floor(Math.random() * charsLength));
+  }
+  return result;
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase};
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
